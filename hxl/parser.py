d0cd1e0930b7f95bc46f57c8a83529c7cc8aed94
David Megginson <david.megginson@megginson.com>
2014-10-10 18:37:00 -0400

    Roughed in literal copy of PHP code, but not working yet.

--- hxl/parser.py
+++ hxl/parser.py
@@ -8,6 +8,7 @@ Documentation: http://hxlstandard.org
 """
 
 import csv
+import re
 
 from model import HXLColumn, HXLRow, HXLValue
 
@@ -70,10 +71,126 @@ class HXLReader:
     def __init__(self, source):
         self.csvreader = csv.reader(source)
 
-    def next(self):
-        return self.csvreader.next()
-
     def __iter__(self):
         return self;
 
+    def next(self):
+        if self.tableSpec == None:
+            self.tableSpec = self.parseTableSpec()
+            self.disaggregationCount = self.tableSpec.getDisaggregationCount()
+            self.disaggregationPosition = 0
+
+        if self.disaggregationPosition >= self.disaggregationCount or not self.rawData:
+            self.rawData = self.parseSourceRow()
+            if (self.rawData == None):
+                return None
+            self.disaggregationPosition = 0
+        ++self.rowNumber
+
+        data = []
+        columnNumber = -1
+        seenFixed = 0
+        n = 0
+        for content in self.rawData:
+            colSpec = self.tableSpec.colSpecs[n]
+
+            if not colSpec.column.hxlTag:
+                continue
+
+            if colSpec.fixedColumn:
+                if not seenFixed:
+                    ++columnNumber
+                    fixedPosition = self.tableSpec.getFixedPosition(self.disaggregationPosition)
+                    data.append(HXLValue(
+                            self.tableSpec.colSpecs[fixedPosition].fixedColumn,
+                            self.tableSpec.colSpecs[fixedPosition].fixedValue,
+                            columnNumber,
+                            sourceColumnNumber
+                            ))
+                    ++columnNumber
+                    data.append(HXLValue(
+                            self.tableSpec.colSpecs[fixedPosition].column,
+                            self.rawData[fixedPosition],
+                            columnNumber,
+                            sourceColumnNumber
+                            ))
+                    seenFixed = true
+                else:
+                    ++columnNumber
+                    data.append(HXLValue(
+                            self.tableSpec.colSpecs[sourceColumnNumber].column,
+                            self.rawData[sourceColumnNumber],
+                            columnNumber,
+                            sourceColumnNumber
+                            ))
+                ++n
+
+        ++self.disaggregationPosition
+        return HXLRow(data, self.rowNumber, self.sourceRowNumber)
+
+    def parseSourceRow(self):
+        ++self.sourceRowNumber
+        return self.csvreader.next()
+
+    def parseTableSpec(self):
+        rawData = self.parseSourceRow()
+        while rawData:
+            tableSpec = self.parseHashtagRow(rawData)
+            if (tableSpec != None):
+                return tableSpec
+            else:
+                self.lastHeaderRow = rawData
+            rawData = self.parseSourceRow()
+        raise Exception("HXL hashtag row not found")
+
     
+    def parseHashtagRow(self, rawDataRow):
+        tableSpec = HXLTableSpec()
+        seenHeader = 0
+        sourceColumnNumber = 0
+        for rawString in rawDataRow:
+            rawString = rawString.strip()
+            if rawString:
+                colSpec = self.parseHashtag(sourceColumnNumber, rawString)
+                if (colSpec):
+                    seenHeader = 1
+                    if (colSpec.fixedColumn):
+                        colSpec.fixedColumn.headerText = self.prettyTag(colSpec.fixedColumn.hxlTag)
+                        colSpec.column.headerText = self.prettyTag(colSpec.column.hxlTag)
+                        colSpec.fixedValue = self.lastHeaderRow[sourceColumnNumber]
+                    else:
+                        colSpec.column.headerText = self.lastHeaderRow[sourceColumnNumber]
+                else:
+                    return None
+            else:
+                colSpec = HXLColSpec(sourceColumnNumber, None, None)
+                colSpec.column = HXLColumn(None, None, None)
+            tableSpec.add(colSpec)
+            ++sourceColumnNumber
+
+        if seenHeader:
+            return tableSpec
+        else:
+            return None
+
+    def parseHashtag(self, sourceColumnNumber, rawString):
+        tagRegexp = '(#[a-zA-z0-9_]+)(?:\/([a-zA-Z]{2}))?'
+        fullRegexp = '^\s*' + tagRegexp + '(?:\s*\+\s*' + tagRegexp + ')?\s*\$';
+        result = re.match(fullRegexp, rawString)
+        if result != None:
+            col1 = HXLColumn(result.group(1), result.group(2))
+            col2 = Null
+            if result.group(3):
+                col2 = HXLColumn(result.group(3), result.group(4))
+                colSpec = HXLColSpec(sourceColumnNumber, col2, col1)
+            else:
+                colSpec = HXLColSpec(sourceColumnNumber, col1, None)
+            return colSpec
+        else:
+            return 0
+
+    def prettyTag(self, hxlTag):
+        hxlTag = re.replace('^#', '', hxlTag)
+        hxlTag = re.replace('_(date|deg|id|link|num)$', '', hxlTag)
+        hxlTag = re.replace('_', ' ', hxlTag)
+        return hxlTag.capitalize()
