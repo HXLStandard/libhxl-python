9ee5e333fbd95d7c22ade88c51acc3d91c2436e3
David Megginson <david.megginson@megginson.com>
2014-10-11 14:08:48 -0400

    Add simple shell script to launch tests.

---
+++ run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+########################################################################
+# Automatically discover and run libhxl_python unit tests.
+#
+# Usage:
+#   sh run-tests.sh
+########################################################################
+
+python -m unittest discover hxl
+
+# end
