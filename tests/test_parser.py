6d94cb6fede2c960b54bab3f2152f648a01a263b
David Megginson <david.megginson@megginson.com>
2014-10-11 15:08:39 -0400

    Parser unit tests complete for now.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -14,6 +14,17 @@ class TestParser(unittest.TestCase):
 
     SAMPLE_FILE = 'sample-data/sample.csv'
     EXPECTED_ROW_COUNT = 8
+    EXPECTED_TAGS = ['#sector', '#subsector', '#org', '#country', '#sex', '#targeted_num', '#adm1'];
+    EXPECTED_CONTENT = [
+        ['WASH', 'Subsector 1', 'Org 1', 'Country 1', 'Males', '100', 'Region 1'],
+        ['WASH', 'Subsector 1', 'Org 1', 'Country 1', 'Females', '100', 'Region 1'],
+        ['Health', 'Subsector 2', 'Org 2', 'Country 2', 'Males', '', 'Region 2'],
+        ['Health', 'Subsector 2', 'Org 2', 'Country 2', 'Females', '', 'Region 2'],
+        ['Education', 'Subsector 3', 'Org 3', 'Country 2', 'Males', '250', 'Region 3'],
+        ['Education', 'Subsector 3', 'Org 3', 'Country 2', 'Females', '300', 'Region 3'],
+        ['WASH', 'Subsector 4', 'Org 1', 'Country 3', 'Males', '80', 'Region 4'],
+        ['WASH', 'Subsector 4', 'Org 1', 'Country 3', 'Females', '95', 'Region 4']
+    ]
 
     def setUp(self):
         self.filename = os.path.join(os.path.dirname(__file__), TestParser.SAMPLE_FILE)
@@ -26,3 +37,16 @@ class TestParser(unittest.TestCase):
             row_count += 1
         self.assertEquals(TestParser.EXPECTED_ROW_COUNT, row_count)
 
+    def test_column_count(self):
+        for row in self.reader:
+            self.assertEquals(len(TestParser.EXPECTED_TAGS), len(row.values))
+
+    def test_columns(self):
+        for row in self.reader:
+            for i, value in enumerate(row):
+                self.assertEquals(TestParser.EXPECTED_TAGS[i], value.column.hxlTag)
+
+    def test_content(self):
+        for i, row in enumerate(self.reader):
+            for j, value in enumerate(row):
+                self.assertEquals(TestParser.EXPECTED_CONTENT[i][j], value.content)
