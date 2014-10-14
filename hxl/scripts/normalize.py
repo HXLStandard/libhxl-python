ddcf68c93706e71f21371f84b917cd6c68b8f44b
David Megginson <david.megginson@megginson.com>
2014-10-13 21:49:28 -0400

    Updated to use as function as well as command-line script.

--- hxl/scripts/normalize.py
+++ hxl/scripts/normalize.py
@@ -8,9 +8,16 @@ Strip columns without hashtags.
 Strip leading and trailing whitespace from values.
 Strip all but one pre-tag header row.
 
-Usage:
+Command-line usage:
 
   python -m hxl.scripts.normalize < DATA_IN.csv > DATA_OUT.csv
+
+Program usage:
+
+  import sys
+  from hxl.scripts.normalize import normalize
+
+  normalize(sys.stdin, sys.stdout)
 
 License: Public Domain
 Documentation: http://hxlstandard.org
@@ -20,15 +27,24 @@ import sys
 from hxl.parser import HXLReader
 from hxl.writer import HXLWriter
 
-parser = HXLReader(sys.stdin)
-writer = HXLWriter(sys.stdout)
+def normalize(input, output):
+    """
+    Normalize a HXL dataset
+    """
 
-is_first = True
-for row in parser:
-    if is_first:
-        writer.writeHeaders(row)
-        writer.writeTags(row)
-        is_first = False
-    writer.writeData(row)
+    parser = HXLReader(input)
+    writer = HXLWriter(output)
+    is_first = True
+
+    for row in parser:
+        if is_first:
+            writer.writeHeaders(row)
+            writer.writeTags(row)
+            is_first = False
+        writer.writeData(row)
+
+# If run as script
+if __name__ == '__main__':
+    normalize(sys.stdin, sys.stdout)
 
 # end
