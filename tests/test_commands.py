a07e383df38c750ddb7f86501e08211b0394f909
David Megginson <david.megginson@megginson.com>
2014-12-19 12:43:58 -0500

    Test at the script-invocation level now.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -8,58 +8,74 @@ License: Public Domain
 
 import unittest
 import os
+import sys
+import subprocess
+import filecmp
+import difflib
+import tempfile
 from StringIO import StringIO
 from hxl.commands.hxlfilter import hxlfilter
 from hxl.commands.hxlvalidate import hxlvalidate
 
-def open_file(name):
-    path = os.path.join(os.path.dirname(__file__), 'files', 'test_commands', name);
-    return open(path, 'r')
+root_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 class TestFilterCommand(unittest.TestCase):
 
     def setUp(self):
-        self.input = open_file('filter-data.csv')
-        self.output = StringIO()
-
-    def test_filter(self):
-        self.assertTrue(True)
-
-class TestValidateCommand(unittest.TestCase):
-
-    def setUp(self):
-        self.null = open(os.devnull, 'w')
         pass
 
-    def test_pattern(self):
+    def test_filter(self):
+        self.assertTrue(try_script(['hxlfilter', '-f', 'org=X'], 'filter-input-01.csv', 'filter-output-01a.csv'))
 
-        # data matches pattern
-        self.assertTrue(hxlvalidate(
-                input=open_file('pattern-data-01a.csv'),
-                output=self.null,
-                schema_input=open_file('pattern-schema-01.csv')
-            ))
 
-        # data doesn't match pattern
-        self.assertFalse(hxlvalidate(
-                input=open_file('pattern-data-01b.csv'),
-                output=self.null,
-                schema_input=open_file('pattern-schema-01.csv')
-            ))
+def try_script(args, input_file, expected_output_file):
+    """
+    Test run a script in its own subprocess.
+    @param args A list of arguments, including the script name first
+    @param input_file The name of the input HXL file in ./files/test_commands/
+    @param expected_output_file The name of the expected output HXL file in ./files/test_commands
+    @return True if the actual output matches the expected output
+    """
 
-    def test_default(self):
+    def resolve(name):
+        """
+        Resolve a file name in the test directory.
+        """
+        return os.path.join(root_dir, 'tests', 'files', 'test_commands', name)
 
-        # data has correct numeric field in default schema
-        self.assertTrue(hxlvalidate(
-                input=open_file('default-data-02a.csv'),
-                output=self.null
-                ))
+    args = list(args)
+    args[0] = os.path.join(root_dir, 'scripts', args[0])
+    input = open(resolve(input_file), 'r')
+    output = tempfile.NamedTemporaryFile(delete=False)
+    try:
+        subprocess.call(args, stdin=input, stdout=output)
+        output.close()
+        result = diff(output.name, resolve(expected_output_file))
+    finally:
+        # Not using with, because Windows won't allow file to be opened twice
+        os.remove(output.name)
+    return result
 
-        # data does not have correct numeric field
-        self.assertFalse(hxlvalidate(
-                input=open_file('default-data-02b.csv'),
-                output=self.null
-                ))
+def diff(file1, file2):
+    """
+    Compare two files, ignoring line end differences
 
+    If there are differences, print them to stderr in unified diff format.
+
+    @param file1 The full pathname of the first file to compare
+    @param file2 The full pathname of the second file to compare
+    @return True if the files are the same, o
+    """
+    with open(file1, 'rt') as input1:
+        with open(file2, 'rt') as input2:
+            diffs = difflib.unified_diff(
+                input1.read().splitlines(),
+                input2.read().splitlines()
+                )
+    no_diffs = True
+    for diff in diffs:
+        no_diffs = False
+        print >>sys.stderr, diff
+    return no_diffs
 
 # end
