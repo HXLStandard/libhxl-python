e1d8caa4c66bb7850b69dda6340e383cfb7f0ea0
David Megginson <david.megginson@megginson.com>
2014-12-23 20:59:11 -0500

    Refactored hxlvalidate to use the run() function.

--- hxl/filters/hxlvalidate.py
+++ hxl/filters/hxlvalidate.py
@@ -17,6 +17,7 @@ Documentation: http://hxlstandard.org
 """
 
 import sys
+import argparse
 from hxl.parser import HXLReader
 from hxl.schema import loadHXLSchema
 
@@ -29,5 +30,42 @@ def hxlvalidate(input=sys.stdin, output=sys.stderr, schema_input=None):
     schema = loadHXLSchema(schema_input)
     schema.callback = callback
     return schema.validate(parser)
+
+def run(args, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
+    """
+    Run hxlvalidate with command-line arguments.
+    @param args A list of arguments, excluding the script name
+    @param stdin Standard input for the script
+    @param stdout Standard output for the script
+    @param stderr Standard error for the script
+    """
+
+    parser = argparse.ArgumentParser(description = 'Validate a HXL dataset.')
+    parser.add_argument(
+        'infile',
+        help='HXL file to read (if omitted, use standard input).',
+        nargs='?',
+        type=argparse.FileType('r'),
+        default=stdin
+        )
+    parser.add_argument(
+        'outfile',
+        help='HXL file to write (if omitted, use standard output).',
+        nargs='?',
+        type=argparse.FileType('w'),
+        default=stdout
+        )
+    parser.add_argument(
+        '-s',
+        '--schema',
+        help='Schema file for validating the HXL dataset (if omitted, use the default core schema).',
+        metavar='schema',
+        type=argparse.FileType('r'),
+        default=None
+        )
+    args = parser.parse_args(args)
+
+    # Call the command function
+    hxlvalidate(input=args.infile, output=args.outfile, schema_input=args.schema)
 
 # end
