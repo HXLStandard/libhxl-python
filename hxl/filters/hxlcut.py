c6894e5e28df1a3d2dc1ece466bc678347bf68ae
David Megginson <david.megginson@megginson.com>
2015-01-08 18:00:42 -0500

    Switched hxlcut to use HXLSource-based filter class.

--- hxl/filters/hxlcut.py
+++ hxl/filters/hxlcut.py
@@ -1,16 +1,9 @@
 """
-Command function to cut columns from a HXL dataset.
+Filter class to cut columns from a HXL dataset.
 David Megginson
 October 2014
 
 Can use a whitelist of HXL tags, a blacklist, or both.
-
-Usage:
-
-  import sys
-  from hxl.scripts.hxlcut import hxlcut
-
-  hxlcut(sys.stdin, sys.stdout, include_tags=['#org', '#country', '#sector'])
 
 License: Public Domain
 Documentation: http://hxlstandard.org
@@ -18,34 +11,39 @@ Documentation: http://hxlstandard.org
 
 import sys
 import argparse
-import csv
-from hxl.parser import HXLReader
+from hxl.model import HXLSource, HXLRow
+from hxl.parser import HXLReader, writeHXL
 from hxl.filters import parse_tags
 
-def hxlcut(input, output, include_tags = [], exclude_tags = []):
-    """
-    Cut columns from a HXL dataset
-    """
+class HXLCutFilter(HXLSource):
 
-    parser = HXLReader(input)
-    writer = csv.writer(output)
+    def __init__(self, source, include_tags=[], exclude_tags=[]):
+        self.source = source
+        self.include_tags = include_tags
+        self.exclude_tags = exclude_tags
+        self.columns_out = None
 
-    tags = parser.tags
+    @property
+    def columns(self):
+        if self.columns_out is None:
+            self.columns_out = []
+            for column in self.source.columns:
+                if self._test_column(column):
+                    self.columns_out.append(column)
+        return self.columns_out
 
-    def restrict_tags(list_in):
-        '''Apply include_tags and exclude_tags to the columns'''
-        list_out = []
-        for i, e in enumerate(list_in):
-            if ((not include_tags) or (tags[i] in include_tags)) and ((not exclude_tags) or (tags[i] not in exclude_tags)):
-                list_out.append(e)
-        return list_out
+    def next(self):
+        row = self.source.next()
+        values_out = []
+        for pos, value in enumerate(row):
+            if self._test_column(row.columns[pos]):
+                values_out.append(value)
+        row_out = HXLRow(self.columns)
+        row_out.values = values_out
+        return row_out
 
-    if parser.hasHeaders:
-        writer.writerow(restrict_tags(parser.headers))
-    writer.writerow(restrict_tags(parser.tags))
-
-    for row in parser:
-        writer.writerow(restrict_tags(row.values))
+    def _test_column(self, column):
+        return ((not self.include_tags) or (column.hxlTag in self.include_tags)) and ((not self.exclude_tags) or (column.hxlTag not in self.exclude_tags))
 
 def run(args, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
     """
@@ -88,6 +86,8 @@ def run(args, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
     args = parser.parse_args(args)
 
     # Call the command function
-    hxlcut(args.infile, args.outfile, include_tags=args.include, exclude_tags=args.exclude)
+    source = HXLReader(args.infile)
+    filter = HXLCutFilter(source, args.include, args.exclude)
+    writeHXL(args.outfile, filter)
 
 # end
