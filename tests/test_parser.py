d45cc4b6b76a921bb0908839578bdab853fcb426
David Megginson <david.megginson@megginson.com>
2015-02-14 08:53:21 -0500

    Use with wrappers to close files.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -40,72 +40,70 @@ class TestParser(unittest.TestCase):
         pass
 
     def test_row_count(self):
-        source = _read_file()
-        # logical row count
         row_count = 0
-        for row in source:
-            row_count += 1
+        with _read_file() as input:
+            # logical row count
+            for row in HXLReader(input):
+                row_count += 1
         self.assertEqual(TestParser.EXPECTED_ROW_COUNT, row_count)
 
     def test_headers(self):
-        source = _read_file()
-        headers = source.headers
+        with _read_file() as input:
+            headers = HXLReader(input).headers
         self.assertEqual(TestParser.EXPECTED_HEADERS, headers)
 
     def test_tags(self):
-        source = _read_file()
-        tags = source.tags
+        with _read_file() as input:
+            tags = HXLReader(input).tags
         self.assertEqual(TestParser.EXPECTED_TAGS, tags)
 
     def test_languages(self):
-        source = _read_file()
-        for row in source:
-            for columnNumber, column in enumerate(row.columns):
-                self.assertEqual(TestParser.EXPECTED_LANGUAGES[columnNumber], column.languageCode)
+        with _read_file() as input:
+            for row in HXLReader(input):
+                for columnNumber, column in enumerate(row.columns):
+                    self.assertEqual(TestParser.EXPECTED_LANGUAGES[columnNumber], column.languageCode)
 
     def test_column_count(self):
-        source = _read_file()
-        for row in source:
-            self.assertEqual(len(TestParser.EXPECTED_TAGS), len(row.values))
+        with _read_file() as input:
+            for row in HXLReader(input):
+                self.assertEqual(len(TestParser.EXPECTED_TAGS), len(row.values))
 
     def test_columns(self):
-        source = _read_file()
-        for row in source:
-            for columnNumber, column in enumerate(row.columns):
-                self.assertEqual(TestParser.EXPECTED_TAGS[columnNumber], column.hxlTag)
+        with _read_file() as input:
+            for row in HXLReader(input):
+                for columnNumber, column in enumerate(row.columns):
+                    self.assertEqual(TestParser.EXPECTED_TAGS[columnNumber], column.hxlTag)
 
     def test_content(self):
-        source = _read_file()
-        for i, row in enumerate(source):
-            for j, value in enumerate(row):
-                self.assertEqual(TestParser.EXPECTED_CONTENT[i][j], value)
+        with _read_file() as input:
+            for i, row in enumerate(HXLReader(input)):
+                for j, value in enumerate(row):
+                    self.assertEqual(TestParser.EXPECTED_CONTENT[i][j], value)
 
     def test_fuzzy(self):
         """Imperfect hashtag row should still work."""
         seen_exception = False
-        try:
-            source = _read_file(TestParser.FILE_FUZZY)
-            source.tags
-        except HXLParseException:
-            seen_exception = True
+        with _read_file(TestParser.FILE_FUZZY) as input:
+            try:
+                HXLReader(input).tags
+            except HXLParseException:
+                seen_exception = True
         self.assertFalse(seen_exception)
 
     def test_invalid(self):
         """Missing hashtag row should raise an exception."""
         seen_exception = False
-        try:
-            source = _read_file(TestParser.FILE_INVALID)
-            source.tags
-        except HXLParseException:
-            seen_exception = True
+        with _read_file(TestParser.FILE_INVALID) as input:
+            try:
+                HXLReader(TestParser.FILE_INVALID).tags
+            except HXLParseException:
+                seen_exception = True
         self.assertTrue(seen_exception)
-            
 
 def _read_file(filename=None):
     """Open a file containing a HXL dataset."""
     if not filename:
         filename = TestParser.FILE_VALID
     absolute_filename = os.path.join(os.path.dirname(__file__), filename)
-    input = open(absolute_filename, 'r')
-    return HXLReader(input)
+    return open(absolute_filename, 'r')
 
