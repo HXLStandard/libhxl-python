628c6bb3127f64982eba1e6de5887334cf2c126c
David Megginson <david.megginson@megginson.com>
2015-10-10 11:44:09 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependency_links=[
 ]
 
 setup(name='libhxl',
-      version='2.1',
+      version='2.2',
       description='Python support for the Humanitarian Exchange Language (HXL).',
       author='David Megginson',
       author_email='contact@megginson.com',
