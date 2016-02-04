ddd4d3d16f512a66c8e22f039c06d5afd8ba9ae3
David Megginson <david.megginson@megginson.com>
2016-02-04 10:13:40 -0500

    Updated and finished API docs.

--- hxl/common.py
+++ hxl/common.py
@@ -1,30 +1,43 @@
 """
-Common information for HXL.
+Common classes, variables, and functions for the HXL library.
 """
 
-import six
 import re
-import sys
 
-TOKEN = r'[A-Za-z][_0-9A-Za-z]*'
+TOKEN_PATTERN = r'[A-Za-z][_0-9A-Za-z]*'
+"""Regular-expression pattern for a single token."""
+
+WHITESPACE_PATTERN = re.compile('\s+', re.MULTILINE)
+"""Regular-expression pattern for multi-line whitespace."""
 
 class HXLException(Exception):
     """Base class for all HXL-related exceptions."""
 
     def __init__(self, message, data={}):
+        """Create a new HXL exception.
+
+        @param message: error message for the exception
+        @param data: dict of properties associated with the exception (default {})
+        """
         super(Exception, self).__init__(message)
         self.message = message
+        """The human-readable error message."""
         self.data = data
+        """Additional properties related to the error."""
 
     def __str__(self):
         return "<HXLException: " + str(self.message) + ">"
 
-WS = re.compile('\s\s+', re.MULTILINE)
-    
 def normalise_string(s):
-    """Normalise a string."""
+    """Normalise a string.  
+
+    Remove all leading and trailing whitespace. Convert to lower
+    case. Replace all internal whitespace (including lineends) with a single space.
+
+    @param s: the string to normalise.
+    """
     if s:
-        return re.sub(WS, ' ', str(s).strip().lower().replace("\n", " "))
+        return re.sub(WHITESPACE_PATTERN, ' ', str(s).strip().lower().replace("\n", " "))
     else:
         return ''
 
