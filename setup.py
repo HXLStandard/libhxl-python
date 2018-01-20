f01413f5e74943e149732140456f01659227a0ff
David Megginson <david.megginson@megginson.com>
2018-01-19 20:52:51 -0500

    Start using a special version number for dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='libhxl',
-      version='4.4',
+      version='4.5dev',
       description='Python support for the Humanitarian Exchange Language (HXL).',
       author='David Megginson',
       author_email='contact@megginson.com',
