867f7ccac616a8d426eea47ca4a44019a7891bf9
David Megginson <david.megginson@megginson.com>
2018-01-30 11:30:29 -0500

    Prepare for 4.5 release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='libhxl',
-      version='4.5dev',
+      version='4.5',
       description='Python support for the Humanitarian Exchange Language (HXL).',
       author='David Megginson',
       author_email='contact@megginson.com',
