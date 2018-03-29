439f42b8e715f8d93e984d0ebd2f3007b3818af6
David Megginson <david.megginson@megginson.com>
2018-03-29 15:37:49 -0400

    Prepare for 4.6 release.
    End legacy support for Python2

--- hxl/__init__.py
+++ hxl/__init__.py
@@ -41,6 +41,11 @@ actions as above in a shell script like this::
 
 """
 
+import sys
+
+if sys.version_info < (3,):
+    raise RuntimeError("libhxl requires Python 3 or higher")
+
 # Flatten out common items for easier access
 
 from hxl.common import HXLException
