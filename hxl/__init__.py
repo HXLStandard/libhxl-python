77ba387f5014b52929644f0ec5b1ef8feb40a13c
David Megginson <david.megginson@gmail.com>
2019-03-04 11:43:45 -0500

    Prepare for 4.14 release

--- hxl/__init__.py
+++ hxl/__init__.py
@@ -45,7 +45,7 @@ import sys
 if sys.version_info < (3,):
     raise RuntimeError("libhxl requires Python 3 or higher")
 
-__version__="4.13.2"
+__version__="4.14"
 """Module version number
 see https://www.python.org/dev/peps/pep-0396/
 """
