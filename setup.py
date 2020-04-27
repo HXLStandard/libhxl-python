d89d3adb161d7c50492b31ed3aab8c18e7f650b4
David Megginson <david.megginson@gmail.com>
2020-04-27 10:57:39 -0400

    Pull README.md in as long description for PyPI.

--- setup.py
+++ setup.py
@@ -6,37 +6,43 @@ import sys
 if sys.version_info < (3,):
     raise RuntimeError("libhxl requires Python 3 or higher")
 
-setup(name='libhxl',
-      version="4.19",
-      description='Python support library for the Humanitarian Exchange Language (HXL). See http://hxlstandard.org and https://github.com/HXLStandard/libhxl-python',
-      author='David Megginson',
-      author_email='megginson@un.org',
-      url='http://hxlproject.org',
-      install_requires=['python-dateutil', 'xlrd', 'requests', 'unidecode', 'python-io-wrapper', 'jsonpath_rw', 'ply'],
-      packages=['hxl', 'hxl.formulas'],
-      package_data={'hxl': ['*.json']},
-      include_package_data=True,
-      test_suite='tests',
-      tests_require = ['mock'],
-      entry_points={
-          'console_scripts': [
-              'hxladd = hxl.scripts:hxladd',
-              'hxlappend = hxl.scripts:hxlappend',
-              'hxlclean = hxl.scripts:hxlclean',
-              'hxlcount = hxl.scripts:hxlcount',
-              'hxlcut = hxl.scripts:hxlcut',
-              'hxldedup = hxl.scripts:hxldedup',
-              'hxlexplode = hxl.scripts:hxlexplode',
-              'hxlfill = hxl.scripts:hxlfill',
-              'hxlimplode = hxl.scripts:hxlimplode',
-              'hxlhash = hxl.scripts:hxlhash',
-              'hxlmerge = hxl.scripts:hxlmerge',
-              'hxlrename = hxl.scripts:hxlrename',
-              'hxlreplace = hxl.scripts:hxlreplace',
-              'hxlselect = hxl.scripts:hxlselect',
-              'hxlsort = hxl.scripts:hxlsort',
-              'hxltag = hxl.scripts:hxltag',
-              'hxlvalidate = hxl.scripts:hxlvalidate'
-          ]
-      }
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='libhxl',
+    version="4.19",
+    description='Python support library for the Humanitarian Exchange Language (HXL). See http://hxlstandard.org and https://github.com/HXLStandard/libhxl-python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='David Megginson',
+    author_email='megginson@un.org',
+    url='http://hxlproject.org',
+    install_requires=['python-dateutil', 'xlrd', 'requests', 'unidecode', 'python-io-wrapper', 'jsonpath_rw', 'ply'],
+    packages=['hxl', 'hxl.formulas'],
+    package_data={'hxl': ['*.json']},
+    include_package_data=True,
+    test_suite='tests',
+    tests_require = ['mock'],
+    entry_points={
+        'console_scripts': [
+            'hxladd = hxl.scripts:hxladd',
+            'hxlappend = hxl.scripts:hxlappend',
+            'hxlclean = hxl.scripts:hxlclean',
+            'hxlcount = hxl.scripts:hxlcount',
+            'hxlcut = hxl.scripts:hxlcut',
+            'hxldedup = hxl.scripts:hxldedup',
+            'hxlexplode = hxl.scripts:hxlexplode',
+            'hxlfill = hxl.scripts:hxlfill',
+            'hxlimplode = hxl.scripts:hxlimplode',
+            'hxlhash = hxl.scripts:hxlhash',
+            'hxlmerge = hxl.scripts:hxlmerge',
+            'hxlrename = hxl.scripts:hxlrename',
+            'hxlreplace = hxl.scripts:hxlreplace',
+            'hxlselect = hxl.scripts:hxlselect',
+            'hxlsort = hxl.scripts:hxlsort',
+            'hxltag = hxl.scripts:hxltag',
+            'hxlvalidate = hxl.scripts:hxlvalidate'
+        ]
+    }
 )
