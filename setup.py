0babff28e04c7da97cae91de78e86295bc42b118
David Megginson <david.megginson@gmail.com>
2020-05-08 13:05:46 -0400

    Switch from jsonpath_rw to jsonpath-rw-ext to support more JSONPath features.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author='David Megginson',
     author_email='megginson@un.org',
     url='http://hxlproject.org',
-    install_requires=['python-dateutil', 'xlrd', 'requests', 'unidecode', 'python-io-wrapper', 'jsonpath_rw', 'ply'],
+    install_requires=['python-dateutil', 'xlrd', 'requests', 'unidecode', 'python-io-wrapper', 'jsonpath-rw-ext', 'ply'],
     packages=['hxl', 'hxl.formulas'],
     package_data={'hxl': ['*.json']},
     include_package_data=True,
