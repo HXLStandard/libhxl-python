d40a7891dffdd4c70e6abaab9c3d9701739a492e
David Megginson <david.megginson@gmail.com>
2020-06-19 13:42:31 -0400

    Improved docstrings for hxl.geo module

--- hxl/geo.py
+++ hxl/geo.py
@@ -1,73 +1,92 @@
 """
 Geodata operations
-David Megginson
-Started February 2018
 
-License: Public Domain
-Documentation: https://github.com/HXLStandard/libhxl-python/wiki
+Functions for parsing latitudes and longitudes
+
+May add more geo ops here later (e.g. filtering by boundaries).
+
+Author:
+    David Megginson
+
+License:
+    Public Domain
+
 """
 
-import re
+import hxl, logging, re
+
+__all__ = ["LAT_PATTERNS", "LON_PATTERNS", "parse_lat", "parse_lon", "parse_coord"]
+
+logger = logging.getLogger(__name__)
+
+
+
+########################################################################
+# Constants
+########################################################################
 
 # regular expression fragments
-DEG_RE = '(?P<deg>\d+(?:\.\d*)?)\s*\°?'
-MIN_RE = '(?P<min>\d+(?:\.\d*)?)\s*[\'`′]?'
-SEC_RE = '(?P<sec>\d+(?:\.\d*)?)\s*(?:["“”″]|[\'`′][\'`′])?'
+_DEG_RE = '(?P<deg>\d+(?:\.\d*)?)\s*\°?'
+_MIN_RE = '(?P<min>\d+(?:\.\d*)?)\s*[\'`′]?'
+_SEC_RE = '(?P<sec>\d+(?:\.\d*)?)\s*(?:["“”″]|[\'`′][\'`′])?'
 
 LAT_PATTERNS = (
     re.compile(
         '^(?P<sign>[+-])?\s*{}(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # -00 00 00
     re.compile(
         '^(?P<hemi>[NS])\s*{}(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # S 00 00 00
     re.compile(
         '^{}\s*(?P<hemi>[NS])\s*(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # 00 S 00 00
     re.compile(
         '^{}(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)\s*(?P<hemi>[NS])?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # 00 00 00 S
 )
-"""Regular expressions for parsing latitude strings"""
+"""List of regular expressions for parsing latitude strings"""
+
 
 LON_PATTERNS = (
     re.compile(
         '^(?P<sign>[+-])?\s*{}(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # -00 00 00
     re.compile(
         '^(?P<hemi>[EW])\s*{}(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # S 00 00 00
     re.compile(
         '^{}\s*(?P<hemi>[EW])\s*(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # 00 S 00 00
     re.compile(
         '^{}(?:[\s:;,-]*{}(?:[\s:;,-]*{})?)\s*(?P<hemi>[EW])?$'.format(
-            DEG_RE, MIN_RE, SEC_RE
+            _DEG_RE, _MIN_RE, _SEC_RE
         ), flags=re.I
     ), # 00 00 00 S
 )
-"""Regular expressions for parsing longitude strings"""
+"""List of regular expressions for parsing longitude strings"""
 
-def _make_num(parts, max_deg):
-    """Parse the parts from a regular expression match into a decimal number.
-    @param parts: the parts from the regex match
-    @param max_deg: the maximum degrees allowed (90 or 180)
-    @returns: a floating point representation
-    @exception: if part of the latitude or longitude is out of range
+
+
+########################################################################
+# Functions
+########################################################################
+
+def _make_degrees_digital(parts, max_deg):
+    """Assemble the degrees, minutes, and seconds parts from a regular expression result into a decimal number.
     """
     num = float(parts['deg'])
     if num > max_deg or num < max_deg*-1:
@@ -87,49 +106,94 @@ def _make_num(parts, max_deg):
         num *= -1
     return num
 
-def parse_lat(s):
+
+def parse_lat(value):
     """Parse a latitude string
-    @param s: an input string to parse
-    @returns: decimal longitude, or None on failure
-    @exception ValueError: if part of the latitud is out of allowed range
+
+    Accepts a wide range of formats, as defined in LAT_PATTERNS
+
+    Examples:
+        ```
+        lat = parse_lat("45.5000000") # => 45.5
+        lat = parse_lat("45:30N") # => 45.5
+        ```
+
+    Args:
+        value (str): the input string to parse
+
+    Returns:
+        float: decimal degrees latitude, or None if it can't be parsed.
+
+    Raises:
+        ValueError: if the input is out of allowed range
+    
     """
-    s = s.strip()
+    value = hxl.datatypes.normalise_space(value)
     for pattern in LAT_PATTERNS:
-        result = re.match(pattern, s)
+        result = re.match(pattern, value)
         if result:
             try:
-                lat = _make_num(result.groupdict(), max_deg=90)
+                lat = _make_degrees_digital(result.groupdict(), max_deg=90)
             except ValueError as e:
-                raise ValueError('failed to parse latitude {}: {}'.format(s, e.args[0]))
+                raise ValueError('failed to parse latitude {}: {}'.format(value, e.args[0]))
             return lat
     return None
 
-def parse_lon(s):
+
+def parse_lon(value):
     """Parse a longitude string
-    @param s: an input string to parse
-    @returns: decimal longitude, or None on failure
-    @exception ValueError: if part of the longitude is out of allowed range
+
+    Accepts a wide range of formats, as defined in LON_PATTERNS
+
+    Examples:
+        ```
+        lon = parse_lon("-75.5000000") # => -75.5
+        lon = parse_lon("75:30W") # => -75.5
+        ```
+
+    Args:
+        value (str): the input string to parse
+
+    Returns:
+        float: decimal degrees longitude, or None if it can't be parsed.
+
+    Raises:
+        ValueError: if the input is out of allowed range
+    
     """
-    s = s.strip()
+    value = hxl.datatypes.normalise_space(value)
     for pattern in LON_PATTERNS:
-        result = re.match(pattern, s)
+        result = re.match(pattern, value)
         if result:
             try:
-                lat = _make_num(result.groupdict(), max_deg=180)
+                lat = _make_degrees_digital(result.groupdict(), max_deg=180)
             except ValueError as e:
-                raise ValueError('failed to parse latitude {}: {}'.format(s, e.args[0]))
+                raise ValueError('failed to parse latitude {}: {}'.format(value, e.args[0]))
             return lat
     return None
 
-def parse_coord(s):
+
+def parse_coord(value):
     """Parse lat/lon separated by a delimiter [/,:; ]
-    @param s: an input string to parse
-    @returns: a tuple with decimal latitude and longitude, or None on failure
-    @exception ValueError: if part of the latitude or longitude is out of allowed range
+
+    Examples:
+        ```
+        coord = parse_coord("45.500000;-75.5000000") # => (45.5, -75.5,)
+        coord = parse_coord("45:30N, 75:30W") # => (45.5, -75.5,)
+        ```
+
+    Args:
+        value (str): the lat/lon coordinate string to parse
+
+    Returns:
+        tuple: the latitude and longitude as float values, or None if unparseable
+
+    Raises:
+        ValueError: if either of the coordinates is out of range
     """
     for delim in ('/', ',', ':', ';', ' ',):
-        if s.find(delim) > 0:
-            parts = s.split(delim)
+        if value.find(delim) > 0:
+            parts = value.split(delim)
             if len(parts) == 2:
                 lat = parse_lat(parts[0])
                 lon = parse_lon(parts[1])
