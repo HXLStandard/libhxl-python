8d32d32c8165ec2ef4f7d970c30ca037fcc46b35
David Megginson <david.megginson@gmail.com>
2021-04-23 10:39:23 -0400

    Bump version to 4.24.1 because of PyPi upload problem.

--- hxl/__init__.py
+++ hxl/__init__.py
@@ -45,7 +45,7 @@ import sys
 if sys.version_info < (3,):
     raise RuntimeError("libhxl requires Python 3 or higher")
 
-__version__="4.24"
+__version__="4.24.1"
 """Module version number
 see https://www.python.org/dev/peps/pep-0396/
 """
